c20dab9dac7446763b2d097f88990d2bf4665d19
yqj-lg <2185136206@qq.com>
2017-09-11 16:28:06 +0800

    init

---
+++ src/Sort.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author Tinker
+ * Date 17-9-11
+ * Time 下午4:25
+ */
+
+
+namespace tinker\sort;
+
+
+class Sort
+{
+    public static function test()
+    {
+        return "hehe";
+    }
+}
